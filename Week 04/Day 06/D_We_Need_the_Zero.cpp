be14d917728db29ea5dda93c2ab05ab9bb422c6b
Anas <163520934+anas-haq@users.noreply.github.com>
2024-04-17 23:49:42 +0600

    Add files via upload
    added day 6 code

---
+++ Week 04/Day 06/D_We_Need_the_Zero.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<int> v(n);        
+        int x=0;
+        for (int i = 0; i < n; i++)
+        {
+            cin >> v[i];
+            
+        }
+        for (int i = 0; i <n; i++)
+        {
+            x^=v[i];
+        }
+        for (int i = 0; i <n; i++)
+        {
+            v[i]^=x;
+        }
+        int y=0;
+        for (int i = 0; i <n; i++)
+        {
+            y^=v[i];
+        }
+        if(y==0)
+        {
+            cout<<x<<'\n';
+        }
+        else
+        {
+            cout<<"-1"<<"\n";
+        }
+        
+        
+        
+    }
+
+    return 0;
+}
