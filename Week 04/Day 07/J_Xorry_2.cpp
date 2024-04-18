d578f482254cb5c64ebf2866eb29f393b3f830ea
Anas <163520934+anas-haq@users.noreply.github.com>
2024-04-18 23:54:15 +0600

    Add files via upload
    day 07 codes added

---
+++ Week 04/Day 07/J_Xorry_2.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int i = 31;
+        int one = 0;
+        while (i >= 0)
+        {
+            if ((n & (1 << i)))
+            {
+                one++;
+            }
+            if (one == 2)
+            {
+                break;
+            }
+            i--;
+        }
+        int cnt = 0;
+        while (i >= 0)
+        {
+            if ((n & (1 << i)) == 0)
+            {
+                cnt++;
+            }
+            i--;
+        }
+        cout << (1 << cnt) << endl;
+    }
+    return 0;
+}
