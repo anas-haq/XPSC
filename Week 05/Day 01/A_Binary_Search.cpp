7dc8aa8c1da21093098fd3567498339e39afbb13
Anas <163520934+anas-haq@users.noreply.github.com>
2024-04-19 20:03:23 +0600

    Add files via upload
    code added

---
+++ Week 05/Day 01/A_Binary_Search.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n, k;
+    cin >> n >> k;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
+    sort(v.begin(), v.end());
+    while (k--)
+    {
+        int x;
+        cin >> x;
+        int l = 0, r = n - 1;
+        bool flag = false;
+        while (l <= r)
+        {
+            int mid = l + (r - l) / 2;
+            if (x == v[mid])
+            {
+                flag = true;
+                break;
+            }
+            else if (x > v[mid])
+            {
+                l = mid + 1;
+            }
+            else
+            {
+                r = mid - 1;
+            }
+        }
+
+        if (flag)
+        {
+            cout << "YES" << '\n';
+        }
+        else
+        {
+            cout << "NO" << '\n';
+        }
+    }
+
+    return 0;
+}
