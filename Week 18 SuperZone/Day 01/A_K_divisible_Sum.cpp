09e0879fd8103834faa2c6944563537b63a35ea6
Anas <163520934+anas-haq@users.noreply.github.com>
2024-08-26 23:13:21 +0600

    A_K_divisible_Sum.cpp
    .

--- Week 18 SuperZone/Day 01/A_K_divisible_Sum.cpp
+++ Week 18 SuperZone/Day 01/A_K_divisible_Sum.cpp
@@ -17,34 +17,21 @@ int main() {
     cin >> t;
 
     auto Anas = [&]() {
-        ll n,k;
-        cin>>n>>k;
-        if(n>k)
+        ll n, k;
+        cin >> n >> k;
+        if (n > k)
         {
-            if(n%k==0)
-            {
-                cout<<n/k<<"\n";
-            }
+            if (n % k == 0)
+                cout << "1\n";
             else
-            {
-                cout<<n/k+1<<"\n";
-            }           
-
-        }
-        else if(n==k)
-        {
-            cout<<1<<"\n";
+                cout << "2\n";
         }
         else
         {
-            if(k%n==0)
-            {
-                cout<<k/n<<"\n";
-            }
+            if (k %n == 0)
+                cout << k / n << "\n";
             else
-            {
-                cout<<k/n+1<<"\n";
-            } 
+                cout << k/n + 1 << "\n";
         }
         
     };
